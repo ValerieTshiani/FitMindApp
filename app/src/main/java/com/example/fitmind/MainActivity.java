9b94c2bc587e24a0199969ab21ff91bb67630d00
Swazikajima <ncbmbo002@myuct.ac.za>
2017-07-01 05:57:15 +0200

    Added a few things for the profile page 2

--- app/src/main/java/com/example/fitmind/MainActivity.java
+++ app/src/main/java/com/example/fitmind/MainActivity.java
@@ -5,11 +5,12 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
