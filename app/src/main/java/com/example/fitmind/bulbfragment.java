dd0b99a7b3ec2deaf69310da7edbc5df854c209f
valerieTshiani <tshval003@myuct.ac.za>
2017-07-01 07:58:47 +0200

    Trophies Page 65% completed

--- app/src/main/java/com/example/fitmind/bulbfragment.java
+++ app/src/main/java/com/example/fitmind/bulbfragment.java
@@ -4,39 +4,96 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.TextView;
 
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class bulbfragment extends Fragment {
 
+    public static subjectAdapter adapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
+    private static RecyclerView recycleView;
 
-     * @return A new instance of fragment bulbfragment.
-     */
-    // TODO: Rename and change types and number of parameters
+    private ArrayList<subject> subjectList;
+
+    private TextView mTextMessage;
+
+    //private ArrayList<subject> subjectList;
+    private subjectAdapter subjectAdapter;
+
     public static bulbfragment newInstance() {
         bulbfragment fragment = new bulbfragment();
         return fragment;
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
+
+
+    String[] subjectNames = {"BIO","Math","English","French"};
+    String[] subjectInfo = {"Birds and the bees ","Numbers","Books","All about language"};
+    int [] pics = {R.drawable.ic_bio,  R.drawable.ic_maths, R.drawable.ic_book,  R.drawable.ic_languages };
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bulbfragment, container, false);
+
+        View view  = inflater.inflate(R.layout.fragment_bulbfragment, container, false);
+        recycleView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        recycleView.setHasFixedSize(true);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 2);
+        recycleView.setLayoutManager(gridLayoutManager);
+        recycleView.setItemAnimator (new DefaultItemAnimator());
+
+
+        //intializing an arraylist called subjectList
+        subjectList = new ArrayList<>();
+        subjectList.add(new subject("Bio","Birds and the bees",R.drawable.ic_bio));
+        subjectList.add(new subject("Maths","Numbers",R.drawable.ic_maths));
+        subjectList.add(new subject("English","Books",R.drawable.ic_book));
+        subjectList.add(new subject("French","OUI OuI",R.drawable.ic_languages));
+
+        adapter = new subjectAdapter(subjectList);
+        recycleView.setAdapter(adapter);
+
+
+      /*  mRecyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this, new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(MainActivity.this, "Card at " + position + " is clicked", Toast.LENGTH_SHORT).show();
+            }
+        })); */
+
+        recycleView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Click pressed",Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        return view;
+
+
+
     }
 
 
